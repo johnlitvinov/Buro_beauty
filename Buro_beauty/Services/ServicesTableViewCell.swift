eea5813af29804582668bc8473c66a7c90ab81ca
John Litvinov <john.litvinov89@gmail.com>
2018-03-25 15:13:58 +0300

    Home page and Services

---
+++ Buro_beauty/Services/ServicesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ServicesTableViewCell.swift
+//  Buro_beauty
+//
+//  Created by Ivan Litvinov on 3/25/18.
+//  Copyright © 2018 Ivan Litvinov. All rights reserved.
+//
+
+import UIKit
+
+class ServicesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Buro_beauty/ViewController.swift
+++ Buro_beauty/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        button1.layer.cornerRadius = 22
+        button2.layer.cornerRadius = 22
+        button3.layer.cornerRadius = 22
+        button4.layer.cornerRadius = 22
+        button5.layer.cornerRadius = 22
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,10 +36,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func services(_ sender: Any) {
+    self.tabBarController?.selectedIndex = 2
+    }
+    
+    @IBAction func portfolio(_ sender: Any) {
+    self.tabBarController?.selectedIndex = 3
+    }
+    
+    @IBAction func aboutUs(_ sender: Any) {
+    self.tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func contactUs(_ sender: Any) {
+    self.tabBarController?.selectedIndex = 4
+    }
+    
 
 }
-
-//rdkjvnsdfkjnvlksdfnlksd
-
-//амотаомтатм
-
